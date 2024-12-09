1a1252b42a82aa8211fbc75b199821ab2bdb4fb7
Akanksha9033 <happychaudhary123spj@gmail.com>
2024-12-09 15:57:21 +0530

    save

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/navbar/navbar';
+import Hero from './components/Hero/Hero';
+import Contact from './components/Contact/Contact';
+import About from './components/About/About';
+import Footer from './components/Footer/Footer';
+import Login from './components/Login/Login';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Training from './components/Training';
 
-function App() {
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Navbar />
+        <Routes>
+          <Route path="/hero" element={<Hero />} />
+          <Route path="/training" element={<Training />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
